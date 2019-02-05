c7620c4b3d6dc136bad0b182b011ea072dfca0f6
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-02-05 18:13:48 -0500

    starting logic for js...

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("HELLO WORLD");
+// console.log("HELLO WORLD");
+//create an array for the squares
+let boxSwitch = [];
+let scoreCard = 0;
+let one = document.querySelector(".one");
+//use Math.random() to create a random integer
+// link random integer to array index
+//when index is called, do something
+//use Math.random and setInterval functions to create random instances of square color
+//have scorecard change with index.html
+//have timer change with index.html
+let two = {
+  boxTwo: function() {
+    document.querySelector(".two");
+    console.log("???");
+  }
+};
