411d4d1fb1f837f3ad35ba37ac47f71f8d758ee6
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-02-06 11:39:42 -0500

    added active classes in css to toggle on and off in js

--- script.js
+++ script.js
@@ -7,15 +7,12 @@ let three = document.querySelector(".three");
 let four = document.querySelector(".four");
 
 //when index is called, do something
-//use Math.random and setInterval functions to create random instances of square color
 //have scorecard change with index.html
 //have timer change with index.html
 
 //when clicked, add box to an array which will check for correction
 
 for (i = 0; i < 10; i++) {
-  //put delay between iteration, or get it to stop and wait for input
-  setInterval(3000);
   function getNum() {
     return Math.floor(Math.random() * (4 - 0)) + 0;
   }
@@ -25,27 +22,26 @@ for (i = 0; i < 10; i++) {
 }
 // link random integer to array index
 if (boxSwitch.length > 0 && boxSwitch[0] == 1) {
-  one.style.backgroundColor = "red";
+  //use Math.random and setInterval functions to create random instances of square color
+  // one.style.backgroundColor = "red";
+  one.classList.toggle("active");
   console.log("Checking the link");
 } else {
+  // setInterval(3000);
   three.style.backgroundColor = "black";
   console.log("Condition not met");
 }
+function playerCheck() {
+  if (boxSwitch === boxCheck) {
+    console.log("issa match");
+  }
+}
 
-//use switch statement?
-// switch (boxSwitch.length > 0 && boxSwitch[x] == 1) {
-//   case boxSwitch[0]:
-//     console.log("testing the output");
-//     break;
-//   case boxSwitch[1]:
-//     console.log("this is case 2");
-//     break;
-//   default:
-//     console.log("this is the default");
-// }
-
-// one.addEventListener("click", function(e) {
-//   e.target.style.backgroundColor = "white";
-// });
+//work with callback toggle
+//think in phases: Pattern, Player, NewRound
+//create method that compares my two arrays(strict comparison)
+//how to push user input into new array
+//relate .one .two .three .four to 0 1 2 3 in an array
+//research array positions and how to call them
 
 //getNum code from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Getting_a_random_integer_between_two_values
