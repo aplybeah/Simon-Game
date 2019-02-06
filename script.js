edd7fec4742a857eec39e4eb953fd0ec93148297
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-02-06 17:38:35 -0500

    considering using map fucnction on my arrays

--- script.js
+++ script.js
@@ -15,27 +15,33 @@ var reset = document.querySelector(".reset");
 
 //when clicked, add box to an array which will check for correction
 function playerCheck() {
-  if (boxSwitch === boxCheck) {
-    console.log("issa match");
+  if (boxSwitch.length === boxCheck.length) {
+    if (boxSwitch === boxCheck) {
+      console.log("issa match");
+    }
   }
 }
 //remove event listener
 function actBox1() {
   one.classList.toggle("active");
+  timer(actBox1, 300);
 }
 function actBox2() {
   two.classList.toggle("active");
+  timer(actBox2, 300);
 }
 function actBox3() {
   three.classList.toggle("active");
+  timer(actBox3, 300);
 }
 function actBox4() {
   four.classList.toggle("active");
+  timer(actBox4, 300);
 }
 function timer() {
   setTimeout(timer, 300);
-  actBox1();
-  console.log("hey");
+  // actBox1();
+  // console.log("hey");
   //recursion function
 }
 
@@ -61,20 +67,15 @@ start.addEventListener("click", function(e) {
   }
   if (boxSwitch.length > 0 && boxSwitch[0] == 0) {
     setTimeout(actBox1, 500);
-    // timer(boxSwitch);
-    console.log("Checking the link");
   }
   if (boxSwitch.length > 0 && boxSwitch[0] == 1) {
     setTimeout(actBox2, 500);
-    // timer(boxSwitch);
   }
   if (boxSwitch.length > 0 && boxSwitch[0] == 2) {
     setTimeout(actBox3, 500);
-    // timer(boxSwitch);
   }
   if (boxSwitch.length > 0 && boxSwitch[0] == 3) {
     setTimeout(actBox4, 500);
-    // timer(boxSwitch);
   }
 });
 
@@ -95,7 +96,6 @@ four.addEventListener("click", function(e) {
   boxCheck.push(3);
   console.log(boxCheck);
 });
-
 //think in phases: Pattern, Player, NewRound
 //how to push user input into new array
 //relate .one .two .three .four to 0 1 2 3 in an array
