8fef80269de81e69170e25dab6cb5b1e7c7369dc
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-02-05 22:10:10 -0500

    attempting to work with arrays to no avail

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// console.log("HELLO WORLD");
-//create an array for the squares
 let boxSwitch = [];
 let boxCheck = [];
 let scoreCard = 0;
@@ -7,20 +5,30 @@ let one = document.querySelector(".one");
 let two = document.querySelector(".two");
 let three = document.querySelector(".three");
 let four = document.querySelector(".four");
-//use Math.random() to create a random integer
-// link random integer to array index
+
 //when index is called, do something
 //use Math.random and setInterval functions to create random instances of square color
 //have scorecard change with index.html
 //have timer change with index.html
-//use a for loop???
+
 //when clicked, add box to an array which will check for correction
+
 function getNum() {
-  min = Math.ceil(0);
-  max = Math.floor(8);
   return Math.floor(Math.random() * (8 - 0)) + 0;
 }
-console.log(getNum());
+// link random integer to array index
+var x = getNum();
+console.log(x);
+
+console.log(boxSwitch.length);
+// for (i = 0; i < 10; i++) {
+//     function addBox() {
+//     return boxSwitch.push(x);
+//   }
+// }
+// console.log(boxSwitch.push(1));
+//use switch statement?
+// switch()
 // one.addEventListener("click", function(e) {
 //   e.target.style.backgroundColor = "white";
 // });
