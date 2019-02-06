205781561807df6cc90b26f34e66c40f7d6e4265
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-02-06 12:19:15 -0500

    added conditionals and loop to the click action on the start button

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let one = document.querySelector(".one");
 let two = document.querySelector(".two");
 let three = document.querySelector(".three");
 let four = document.querySelector(".four");
+var start = document.querySelector(".start-button");
 
 //when index is called, do something
 //have scorecard change with index.html
@@ -12,31 +13,32 @@ let four = document.querySelector(".four");
 
 //when clicked, add box to an array which will check for correction
 
-for (i = 0; i < 10; i++) {
-  function getNum() {
-    return Math.floor(Math.random() * (4 - 0)) + 0;
+start.addEventListener("click", function(e) {
+  for (i = 0; i < 3; i++) {
+    function getNum() {
+      return Math.floor(Math.random() * (4 - 0)) + 0;
+    }
+    var x = getNum();
+    boxSwitch.push(x);
+    console.log(boxSwitch);
   }
-  var x = getNum();
-  boxSwitch.push(x);
-  console.log(boxSwitch);
-}
+  if (boxSwitch.length > 0 && boxSwitch[0] == 0) {
+    //use Math.random and setInterval functions to create random instances of square color
+    // one.style.backgroundColor = "red";
+    one.classList.toggle("active");
+    console.log("Checking the link");
+  } else {
+    // setInterval(3000);
+    three.style.backgroundColor = "black";
+    console.log("Condition not met");
+  }
+});
 // link random integer to array index
-if (boxSwitch.length > 0 && boxSwitch[0] == 1) {
-  //use Math.random and setInterval functions to create random instances of square color
-  // one.style.backgroundColor = "red";
-  one.classList.toggle("active");
-  console.log("Checking the link");
-} else {
-  // setInterval(3000);
-  three.style.backgroundColor = "black";
-  console.log("Condition not met");
-}
 function playerCheck() {
   if (boxSwitch === boxCheck) {
     console.log("issa match");
   }
 }
-
 //work with callback toggle
 //think in phases: Pattern, Player, NewRound
 //create method that compares my two arrays(strict comparison)
