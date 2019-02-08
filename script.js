4b4186533e5c63105b53edf1d37ef0ceb2c2f3b4
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-02-08 10:41:00 -0500

    going to try to work with better selectors for the squares

--- script.js
+++ script.js
@@ -2,56 +2,52 @@ let boxSwitch = [];
 let boxCheck = [];
 let scoreCard = 0;
 let score = document.querySelector(".score");
-// let button = document.querySelectorAll(".Square");
+let button = document.querySelectorAll(".square");
 let one = document.querySelector(".one");
 let two = document.querySelector(".two");
 let three = document.querySelector(".three");
 let four = document.querySelector(".four");
 var start = document.querySelector(".start-button");
 var reset = document.querySelector(".reset");
+var turnCounter = 0;
 
-//when index is called, do something
-//have scorecard change with index.html
-//have timer change with index.html
-
-function actBox1() {
-  one.classList.add("active");
-  timer(actBox1);
-  // setTimeout(actBox1, 1500);
-}
-function actBox2() {
-  two.classList.add("active");
-  timer(actBox2);
-  // setTimeout(actBox2, 500);
-}
-function actBox3() {
-  three.classList.add("active");
-  timer(actBox3);
-  // setTimeout(actBox3, 500);
-}
-function actBox4() {
-  four.classList.add("active");
-  timer(actBox4);
-  // setTimeout(actBox4, 500);
-}
-function timer() {
-  clock = setTimeout(timer, 300);
-  clock;
-  // console.log("hey");
-}
-function stopTimer() {
-  clearTimeout(clock);
-}
+// function actBox1() {
+//   one.classList.add("active");
+//   // timer(actBox1);
+//   // setTimeout(actBox1, 500);
+// }
+// function actBox2() {
+//   two.classList.add("active");
+//   // timer(actBox2);
+//   // setTimeout(actBox2, 500);
+// }
+// function actBox3() {
+//   three.classList.add("active");
+//   // timer(actBox3);
+//   // setTimeout(actBox3, 500);
+// }
+// function actBox4() {
+//   four.classList.add("active");
+//   // timer(actBox4);
+//   // setTimeout(actBox4, 500);
+// }
+// function timer() {
+//   clock = setTimeout(timer, 300);
+//   clock;
+//   // console.log("hey");
+// }
+// function stopTimer() {
+//   clearTimeout();
+// }
+//make another for loop, with increasing timeout, with the index
 
 reset.addEventListener("click", function(e) {
-  stopTimer();
   boxSwitch = [];
   boxCheck = [];
   one.classList.remove("active");
   two.classList.remove("active");
   three.classList.remove("active");
   four.classList.remove("active");
-  // button.classList.remove("active");
   console.log("this is clicked");
 });
 //userinput and recursion
@@ -62,32 +58,32 @@ start.addEventListener("click", function(e) {
       return Math.floor(Math.random() * (4 - 0)) + 0;
     }
     var x = getNum();
+    boxSwitch.push(x);
     linkAttempt(x);
-    // boxSwitch.push(x);
+    console.log(boxSwitch);
     // boxSwitch.forEach(linkAttempt);
   }
-  console.log(boxSwitch);
+  // console.log(boxSwitch);
 });
 
 function linkAttempt(x) {
-  // console.log("testing the forEach loop");
   if (x == 0) {
     actBox1();
-    boxSwitch.push(0);
+    // boxSwitch.push(0);
     console.log("zero");
   }
   if (x == 1) {
     actBox2();
-    boxSwitch.push(1);
+    // boxSwitch.push(1);
     console.log("one");
   }
   if (x == 2) {
-    boxSwitch.push(2);
+    // boxSwitch.push(2);
     console.log("two");
     actBox3();
   }
   if (x == 3) {
-    boxSwitch.push(3);
+    // boxSwitch.push(3);
     console.log("three");
     actBox4();
   }
@@ -96,15 +92,30 @@ function addScore() {
   scoreCard += 1;
   score.innerHTML = scoreCard;
 }
-//array.length
+function wrongAnswer() {
+  scoreCard -= 1;
+  score.innerHTML = scoreCard;
+}
+
 function playerCheck() {
   if (boxSwitch.length > 2 && boxCheck.length > 2) {
     if (boxSwitch.indexOf(i) === boxCheck.indexOf(i)) {
+      // boxSwitch[i] === boxCheck[i]
       addScore();
       console.log("its a match");
+      turnCounter += 1;
+      console.log(turnCounter);
     } else {
+      wrongAnswer();
       console.log("wrong");
+      turnCounter += 1;
+      console.log(turnCounter);
     }
+  }
+}
+function newTurn() {
+  if (turnCounter > 0) {
+    boxCheck = [];
   }
 }
 
